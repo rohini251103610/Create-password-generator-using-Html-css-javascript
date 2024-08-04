c390497288c5a254360baa1793c6da8e23e826b4
Rohini.R <rohinirachelr@gmail.com>
2024-08-04 21:10:04 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,11 @@
+document.getElementById('generateBtn').addEventListener('click', generatePassword);
+
+function generatePassword() {
+    const length = 12;
+    const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+~`|}{[]:;?><,./-=";
+    let password = "";
+    for (let i = 0, n = charset.length; i < length; ++i) {
+        password += charset.charAt(Math.floor(Math.random() * n));
+    }
+    document.getElementById('password').value = password;
+}
